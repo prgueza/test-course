b681f6d03921d2ccdd5b40ecd026209bea627a7a
Pedro Rodriguez Alia <prodrigueza@minsait.com>
2020-02-03 09:45:01 +0100

    Archivo de configuración

--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,18 @@ module.exports = {
     '.*\\.(vue)$': 'vue-jest',
   },
   testRegex: '\\.spec\\.js$',
-  testPathIgnorePatterns: [ 'functions' ],
+  testPathIgnorePatterns: [
+    // 'functions', // Comentar en parte de Jest
+    'Button', // Comentar en parte de Jest Vue
+    'examples'
+   ],
   coverageDirectory: path.resolve(__dirname, 'reports/unit/coverage'),
-  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**', '!functions/**/*.js'],
+  collectCoverageFrom: [
+    '**/*.{js,vue}',
+    '!**/node_modules/**',
+    // '!functions/**/*.js', // Comentar en parte de Jest
+    '!components/**', // Comentar en parte de Jest
+    '!examples/**'
+  ],
   verbose: true,
 }
